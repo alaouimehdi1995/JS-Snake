bae93f0b2e6335def460e8df4a2c4b9dd0777229
Mehdi ALAOUI <alaoui.mehdi.1995@gmail.com>
2017-03-26 12:34:39 +0100

    Adapted namespaces code style

--- assets/js/snake.js
+++ assets/js/snake.js
@@ -1,27 +1,150 @@
 /**
  * Created by mehdi on 17/03/17.
  */
-var windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 
-var windowHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
 
-var blocSize;
-var leftBorder;
-var rightBorder;
-var topBorder;
-var downBorder;
-var adaptedWidth;
-var adaptedHeight;
-var score=0;
-function adaptToScreen(){
-    blocSize=20;
-    adaptedWidth=parseInt(parseFloat(getComputedStyle(document.getElementById('snake'),null).width)/blocSize)*blocSize+"px";
-    adaptedHeight=parseInt(parseFloat(getComputedStyle(document.getElementById('snake'),null).height)/blocSize)*blocSize+"px";
-    leftBorder=0;
-    topBorder=0;
-    rightBorder=parseFloat(adaptedWidth)/blocSize;
-    downBorder=parseFloat(adaptedHeight)/blocSize;
-}
+var screen={
+    blocSize:20,
+
+    leftBorder:0,
+
+    rightBorder:0,
+
+    topBorder:0,
+
+    downBorder:0,
+
+    adaptedWidth:0,
+
+    adaptedHeight:0,
+
+    adaptToScreen:function (){
+
+        this.adaptedWidth=parseInt(parseFloat(getComputedStyle(document.getElementById('snake'),null).width)/this.blocSize)*this.blocSize+"px";
+        this.adaptedHeight=parseInt(parseFloat(getComputedStyle(document.getElementById('snake'),null).height)/this.blocSize)*this.blocSize+"px";
+        this.leftBorder=0;
+        this.topBorder=0;
+        this.rightBorder=parseFloat(this.adaptedWidth)/this.blocSize;
+        this.downBorder=parseFloat(this.adaptedHeight)/this.blocSize;
+    }
+};
+
+
+var game={
+
+    score:0,
+
+    snake:[{x:2,y:2},{x:2,y:1},{x:1,y:1},{x:0,y:1},{x:0,y:0}],
+
+    direction:"R",
+
+    meal:{
+
+        x:0,
+        y:0,
+
+        randomIntFromInterval:function (min,max) {
+            return (Math.random()*(max-min)+min);
+        },
+
+
+        getRandomBloc:function (){
+            var px=parseInt(this.randomIntFromInterval(screen.leftBorder,screen.rightBorder));
+            var py=parseInt(this.randomIntFromInterval(screen.topBorder,screen.downBorder));
+            this.x=px;this.y=py;
+        },
+        draw:function(){
+            var x=parseInt(this.x)*parseInt(screen.blocSize)+"px";
+            var y=parseInt(this.y)*parseInt(screen.blocSize)+"px";
+            var Div=document.createElement('div');
+            Div.className="bloc";
+            Div.style.top=y;
+            Div.style.left=x;
+            Div.innerHTML=' ';
+            document.getElementById('snake').appendChild(Div);
+        }
+    },
+
+    draw:function (){
+        var x,y;
+        for(var i=0;i<this.snake.length;i++){
+            x=parseInt(this.snake[i].x)*parseInt(screen.blocSize)+"px";
+            y=parseInt(this.snake[i].y)*parseInt(screen.blocSize)+"px";
+            var Div=document.createElement('div');
+            Div.className="bloc";
+            Div.style.top=y;
+            Div.style.left=x;
+            Div.innerHTML=' ';
+            document.getElementById('snake').appendChild(Div);
+        }
+    },
+
+    cleanScreen:function (){
+        var oldDiv=document.getElementById('snake');
+        var parent=oldDiv.parentNode;
+        var newDiv=document.createElement('div');
+        newDiv.id="snake";
+        newDiv.style.width=screen.adaptedWidth;
+        newDiv.style.height=screen.adaptedHeight;
+        newDiv.className="snake";
+        parent.replaceChild(newDiv,oldDiv);
+    
+    },
+
+    play:function () {
+        var head;
+        if (this.direction == "R") {
+            if(this.snake[0].x<screen.rightBorder-1)
+                head = {x: this.snake[0].x + 1, y: this.snake[0].y};
+            else
+                head = {x: screen.leftBorder, y: this.snake[0].y};
+        }
+        else if (this.direction == "L") {
+            if(this.snake[0].x>screen.leftBorder)
+                head = {x: this.snake[0].x - 1, y: this.snake[0].y};
+            else
+                head = {x: screen.rightBorder-1, y: this.snake[0].y};
+        }
+        else if (this.direction == "U") {
+            if(this.snake[0].y>screen.topBorder)
+                head = {x: this.snake[0].x, y: this.snake[0].y - 1};
+            else
+                head = {x: this.snake[0].x, y: screen.downBorder-1};
+        }
+        else if (this.direction == "D") {
+            if(this.snake[0].y<screen.downBorder-1)
+                head = {x: this.snake[0].x, y: this.snake[0].y + 1};
+            else
+                head = {x: this.snake[0].x, y: screen.topBorder};
+        }
+        this.snake.pop();
+        this.snake.unshift(head);
+        this.cleanScreen();
+        this.draw();
+        if(this.checkGameOver()){
+            alert("Game Over");
+        }
+        if(this.snake[0].x==this.meal.x && this.snake[0].y==this.meal.y){
+            this.snake.unshift({x:this.meal.x,y:this.meal.y});
+            this.score+=this.snake.length;
+            document.getElementById("score").innerHTML="Score: "+this.score;
+            this.meal.getRandomBloc();
+
+        }
+        this.meal.draw();
+    },
+
+    checkGameOver:function (){
+        for(i=1;i<this.snake.length;i++){
+            if(this.snake[i].x==this.snake[0].x && this.snake[i].y==this.snake[0].y){
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+
 
 
 
@@ -30,120 +153,29 @@ function listenKeyboard(){
     document.addEventListener('keydown',function(e){
         var code=e.keyCode;
         if(code=="37"){ //gauche
-            if(direction!="R")
-                direction="L";
+            if(game.direction!="R")
+                game.direction="L";
         }
         else if(code=="38"){ //haut
-            if(direction!="D")
-                direction="U";
+            if(game.direction!="D")
+                game.direction="U";
         }
         else if(code=="39"){ //droite
-            if(direction!="L")
-                direction="R";
+            if(game.direction!="L")
+                game.direction="R";
         }
         else if(code=="40"){ //bas
-            if(direction!="U")
-                direction="D";
+            if(game.direction!="U")
+                game.direction="D";
         }
 
     },false);
 }
 
 
-function draw(toDraw){
-    var x,y;
-    for(var i=0;i<toDraw.length;i++){
-
-        x=parseInt(toDraw[i].x)*parseInt(blocSize)+"px";
-        y=parseInt(toDraw[i].y)*parseInt(blocSize)+"px";
-        var Div=document.createElement('div');
-        Div.className="bloc";
-        Div.style.top=y;
-        Div.style.left=x;
-        Div.innerHTML=' ';
-        document.getElementById('snake').appendChild(Div);
-    }
-}
 
 
-function cleanScreen(){
 
-    var oldDiv=document.getElementById('snake');
-    var newDiv=document.createElement('div');
-    newDiv.id="snake";
-    newDiv.style.width=adaptedWidth;
-    newDiv.style.height=adaptedHeight;
-    newDiv.className="snake";
-    document.body.replaceChild(newDiv,oldDiv);
-
-}
-
-
-function randomIntFromInterval(min,max) {
-    return (Math.random()*(max-min)+min);
-}
-
-
-function getRandomBloc(){
-    var px=parseInt(randomIntFromInterval(leftBorder,rightBorder));
-    var py=parseInt(randomIntFromInterval(topBorder,downBorder));
-    return [{x:px,y:py}];
-}
-
-
-function play() {
-    var head;
-
-    if (direction == "R") {
-        if(snake[0].x<rightBorder-1)
-            head = {x: snake[0].x + 1, y: snake[0].y};
-        else
-            head = {x: leftBorder, y: snake[0].y};
-    }
-    else if (direction == "L") {
-        if(snake[0].x>leftBorder)
-            head = {x: snake[0].x - 1, y: snake[0].y};
-        else
-            head = {x: rightBorder-1, y: snake[0].y};
-    }
-    else if (direction == "U") {
-        if(snake[0].y>topBorder)
-            head = {x: snake[0].x, y: snake[0].y - 1};
-        else
-            head = {x: snake[0].x, y: downBorder-1};
-    }
-    else if (direction == "D") {
-        if(snake[0].y<downBorder-1)
-            head = {x: snake[0].x, y: snake[0].y + 1};
-        else
-            head = {x: snake[0].x, y: topBorder};
-    }
-    snake.pop();
-    snake.unshift(head);
-    cleanScreen();
-    draw(snake);
-    if(checkGameOver(snake)){
-        alert("Game Over");
-    }
-    if(snake[0].x==meal[0].x && snake[0].y==meal[0].y){
-        snake.unshift(meal[0]);
-        score+=snake.length;
-        document.getElementById("score").innerHTML="Score: "+score;
-        meal=getRandomBloc();
-
-    }
-    draw(meal);
-}
-
-
-function checkGameOver(toCheck){
-    for(i=1;i<toCheck.length;i++){
-        if(toCheck[i].x==toCheck[0].x && toCheck[i].y==toCheck[0].y){
-            return true;
-        }
-    }
-    return false;
-}
 
 
 /*
@@ -151,16 +183,9 @@ function checkGameOver(toCheck){
 * */
 
 
-adaptToScreen();
-document.getElementById('snake').style.width=adaptedWidth;
-document.getElementById('snake').style.height=adaptedHeight;
-var snake=[{x:2,y:2},{x:2,y:1},{x:1,y:1},{x:0,y:1},{x:0,y:0}];
-
-
-var direction="R";
-var meal=getRandomBloc();
+screen.adaptToScreen();
 listenKeyboard();
-
-var kda=play;
-setInterval(kda, 60);
-
+game.meal.getRandomBloc();
+document.getElementById('snake').style.width=screen.adaptedWidth;
+document.getElementById('snake').style.height=screen.adaptedHeight;
+setInterval(function(){game.play()}, 80);
